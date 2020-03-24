978f4a31ffb6eb52c58100583c57810f1d547903
Lucas Borges <lucas.s.m.b@hotmail.com>
2020-03-24 11:45:28 -0300

    first commit, backend created, frontend setup

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,25 @@
+const crypto = require('crypto');
+const connection = require('../database/connection');
+
+module.exports = {
+    async index (_req, res) {
+        const ngos = await connection('ngos').select('*');
+    
+        return res.json(ngos);
+    },
+    async create(req, res) {
+        const { name, email, whatsapp, city, uf } = req.body;
+        const id = crypto.randomBytes(4).toString('HEX');
+
+        await connection('ngos').insert({
+            id,
+            name,
+            email,
+            whatsapp,
+            city,
+            uf
+        });
+
+        return res.json({ id });
+    }
+}
